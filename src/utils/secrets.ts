fc51020baf5dcb49c75dbaa1ab561145d9529c4a
Owen Young <theowenyoung@gmail.com>
2021-01-12 04:10:23 +0800

    fix: github runner bug

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -4,6 +4,10 @@ export default function getSecrets(): Record<string, string> {
   const githubToken = core.getInput("github-token") || "";
   const secretJson = jsonSecrets || process.env.JSON_SECRETS || "{}";
   const secrets = JSON.parse(secretJson) as Record<string, string>;
+  if (secrets && secrets.github_token) {
+    // gihtub bug, github_token as secrets
+    secrets.GITHUB_TOKEN = secrets.github_token;
+  }
   if (!secrets.GITHUB_TOKEN) {
     secrets.GITHUB_TOKEN =
       githubToken || (process.env.GITHUB_TOKEN as string) || "";
